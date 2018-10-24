832d30da8b4d5864fe1d5a7472fb0459f626a725
awhardy <hardy.andreww@gmail.com>
2018-10-24 14:16:47 -0500

    ProgramsRunsWithAccessId

--- OpenWeatherMapAPI/Program.cs
+++ OpenWeatherMapAPI/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Net;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace OpenWeatherMapAPI
 {
@@ -12,12 +13,15 @@ namespace OpenWeatherMapAPI
             Console.WriteLine("Welcome to OpenWeatherMap");
             Console.WriteLine("Please enter the zip code of the area you want to know the temperature in.\nE.g., 12345");
 
+            string jsonFromSettingsFile = File.ReadAllText("appsettings.development.json");
+            string AccessID = JObject.Parse(jsonFromSettingsFile)["AccessID"].ToString();
+
             while (true)
             {
                 Console.WriteLine("Enter zip");
                 WebClient webClient = new WebClient();
                 string zip = Console.ReadLine();
-                string response = webClient.DownloadString("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "AccessID");
+                string response = webClient.DownloadString("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + AccessID);
                 JObject jo = JObject.Parse(response);
                 JToken degreesKelvin = jo.GetValue("main")["temp"];
 
